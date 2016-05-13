e832c2878718f35cce1ba2f0a9bba9940a1f961e
brusteca <bruno@monkeylearn.com>
2016-05-13 11:23:21 -0300

    Added a spider for a single hotel

--- hotel_sentiment/spiders/booking_single_hotel_spider.py
+++ hotel_sentiment/spiders/booking_single_hotel_spider.py
@@ -2,31 +2,15 @@ import scrapy
 from scrapy.loader import ItemLoader
 from hotel_sentiment.items import BookingReviewItem
 
-#crawl up to 6 pages of review per hotel
-max_pages_per_hotel = 6
 
 class BookingSpider(scrapy.Spider):
-    name = "booking"
+    name = "booking_singlehotel"
     start_urls = [
-        #"http://www.booking.com/searchresults.html?aid=357026&label=gog235jc-city-XX-us-newNyork-unspec-uy-com-L%3Axu-O%3AosSx-B%3Achrome-N%3Ayes-S%3Abo-U%3Ac&sid=b9f9f1f142a364f6c36f275cfe47ee55&dcid=4&city=20088325&class_interval=1&dtdisc=0&from_popular_filter=1&hlrd=0&hyb_red=0&inac=0&label_click=undef&nflt=di%3D929%3Bdistrict%3D929%3B&nha_red=0&postcard=0&redirected_from_city=0&redirected_from_landmark=0&redirected_from_region=0&review_score_group=empty&room1=A%2CA&sb_price_type=total&score_min=0&ss_all=0&ssb=empty&sshis=0&rows=15&tfl_cwh=1",
-        #add your city url here
+        #add your url here
     ]
 
-    pageNumber = 1
-
-    #for every hotel
-    def parse(self, response):
-        for hotelurl in response.xpath('//a[@class="hotel_name_link url"]/@href'):
-            url = response.urljoin(hotelurl.extract())
-            yield scrapy.Request(url, callback=self.parse_hotel)
-
-        next_page = response.xpath('//a[starts-with(@class,"paging-next")]/@href')
-        if next_page:
-            url = response.urljoin(next_page[0].extract())
-            yield scrapy.Request(url, self.parse)
-
     #get its reviews page
-    def parse_hotel(self, response):
+    def parse(self, response):
         reviewsurl = response.xpath('//a[@class="show_all_reviews_btn"]/@href')
         url = response.urljoin(reviewsurl[0].extract())
         self.pageNumber = 1
@@ -34,8 +18,6 @@ class BookingSpider(scrapy.Spider):
 
     #and parse the reviews
     def parse_reviews(self, response):
-        if self.pageNumber > max_pages_per_hotel:
-            return
         for rev in response.xpath('//li[starts-with(@class,"review_item")]'):
             item = BookingReviewItem()
             #sometimes the title is empty because of some reason, not sure when it happens but this works
@@ -55,6 +37,5 @@ class BookingSpider(scrapy.Spider):
 
         next_page = response.xpath('//a[@id="review_next_page_link"]/@href')
         if next_page:
-            self.pageNumber += 1
             url = response.urljoin(next_page[0].extract())
             yield scrapy.Request(url, self.parse_reviews)
