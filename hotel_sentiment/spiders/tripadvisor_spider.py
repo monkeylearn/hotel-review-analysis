5d168068e17f55f0b8f6040fd3533c7a05ab92a4
brusteca <bruno@monkeylearn.com>
2017-08-16 19:07:39 -0300

    updated tripadvisor_spider

--- hotel_sentiment/spiders/tripadvisor_spider.py
+++ hotel_sentiment/spiders/tripadvisor_spider.py
@@ -1,7 +1,10 @@
 import scrapy
 from hotel_sentiment.items import HotelSentimentItem
+import re
 
-#TODO use loaders
+# TODO use loaders
+
+
 class TripadvisorSpider(scrapy.Spider):
     name = "tripadvisor"
     start_urls = [
@@ -9,31 +12,40 @@ class TripadvisorSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for href in response.xpath('//div[@class="listing_title"]/a/@href'):
+        for href in response.xpath('//a[@class="property_title"]/@href'):
             url = response.urljoin(href.extract())
             yield scrapy.Request(url, callback=self.parse_hotel)
 
-        next_page = response.xpath('//div[@class="unified pagination standard_pagination"]/child::*[2][self::a]/@href')
-        if next_page:
-            url = response.urljoin(next_page[0].extract())
-            yield scrapy.Request(url, self.parse)
+        # tripadvisor now has a weird pagination js thingie that doesn't even modify the url
+        # if you feel like finding a solution please do
+
+        # next_page = response.xpath('//div[@class="unified pagination standard_pagination"]/child::*[2][self::a]/@href')
+        # if next_page:
+        #     url = response.urljoin(next_page[0].extract())
+        #     yield scrapy.Request(url, self.parse)
 
     def parse_hotel(self, response):
         for href in response.xpath('//div[starts-with(@class,"quote")]/a/@href'):
             url = response.urljoin(href.extract())
             yield scrapy.Request(url, callback=self.parse_review)
 
-        next_page = response.xpath('//div[@class="unified pagination "]/child::*[2][self::a]/@href')
-        if next_page:
-            url = response.urljoin(next_page[0].extract())
-            yield scrapy.Request(url, self.parse_hotel)
+        # haha fuck you tripadvisor pagination I'm better than you
+        url = response.url
+        if not re.findall(r'or\d', url):
+            next_page = re.sub(r'(-Reviews-)', r'\g<1>or5-', url)
+        else:
+            pagenum = int(re.findall(r'or(\d+)-', url)[0])
+            pagenum_next = pagenum + 5
+            next_page = url.replace('or' + str(pagenum), 'or' + str(pagenum_next))
+        yield scrapy.Request(
+            next_page,
+            meta={'dont_redirect': True},
+            callback=self.parse_hotel
+        )
 
-
-    #to get the full review content I open its page, because I don't get the full content on the main page
-    #there's probably a better way to do it, requires investigation
     def parse_review(self, response):
         item = HotelSentimentItem()
-        item['title'] = response.xpath('//div[@class="quote"]/text()').extract()[0][1:-1] #strip the quotes (first and last char)
+        item['title'] = response.xpath('//div[@class="quote"]/text()').extract()[0][1:-1]  # strip the quotes
         item['content'] = response.xpath('//div[@class="entry"]/p/text()').extract()[0]
-        item['stars'] = response.xpath('//span[@class="rate sprite-rating_s rating_s"]/img/@alt').extract()[0]
+        item['stars'] = response.xpath('//span[starts-with(@class, "rating")]/span/@alt').extract()[0].replace('bubble', 'star')
         return item
